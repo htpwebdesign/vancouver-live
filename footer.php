88a5124cb52ac9743b1e093011b05efb083e6490
Justin Yu <justinxyu1128@gmail.com>
2023-11-20 11:17:03 -0800

    footer semantic fixes

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@
 	if(function_exists ('get_field')) {
 		if ( ! is_post_type_archive('vanlive-vendor') ) {?>
 			<section class="site-vendor-showcase">
-				<section>
-					<h3><?php the_field('vendor_showcase_heading', 'option'); ?></h2>
+				<div>
+					<h2><?php the_field('vendor_showcase_heading', 'option'); ?></h2>
 					<?php
 					$args = array(
 							'post_type'      => 'vanlive-vendor',
@@ -42,12 +42,12 @@
 						wp_reset_postdata();
 					}
 					?>
-				</section>
-				<section>
-					<h3><?php the_field('vendor_cta_heading', 'option'); ?></h3>
+				</div>
+				<div>
+					<h2><?php the_field('vendor_cta_heading', 'option'); ?></h2>
 					<p><?php the_field('vendor_cta_text', 'option'); ?></p>
 					<a href="<?php echo get_site_url(); ?>/vendors#gform_wrapper_2"><?php the_field('vendor_cta_link_text', 'option'); ?></a>
-				</section>
+				</div>
 			
 			</section><!-- .site-vendor-showcase -->
 			<?php 
@@ -55,7 +55,7 @@
 		else {
 			?>
 			<section class="site-vendor-showcase">
-				<h3><?php the_field('vendor_showcase_heading', 'option'); ?></h3>
+				<h2><?php the_field('vendor_showcase_heading', 'option'); ?></h2>
 				<?php gravity_form( 2, false, false, false, false, true ); ?>
 			</section><!-- .site-vendor-showcase -->
 			<?php 
@@ -65,27 +65,27 @@
 		
 		
 			<section class="site-social-media">
-				<section>
-					<h3><?php the_field('subscribe_heading', 'option'); ?></h3>
+				<div>
+					<h2><?php the_field('subscribe_heading', 'option'); ?></h2>
 					<?php gravity_form( 1, false, false, false, false, true ); ?>
-				</section>
-				<section>
+				</div>
+				<div>
 					<p><?php the_field('social_media_heading', 'option'); ?></p>
 					<nav>
 						<a href="<?php the_field('facebook', 'option'); ?> "><?php get_template_part( 'icons/facebook' ); ?></a>
 						<a href="<?php the_field('twitter', 'option'); ?> "><?php get_template_part( 'icons/twitterx' ); ?></a>
 						<a href="<?php the_field('instagram', 'option'); ?> "><?php get_template_part( 'icons/instagram' ); ?></a>
 					</nav>
-				</section>
+				</div>
 			</section>
 	<?php 
 	} 
 	?>
-		<section class="site-copyright">
-			<p><a href="#">Privacy Policy</a> | <a href="#">Terms and Conditions</a> | <a href="#">Contact Us</a></p>
-			<p>Built by <a href="#">Taylor Hillier</a>, <a href="#">Zeinab Kordeh</a>, <a href="#">Justin Yu</a>, and <a href="#">Bruce Gerona</a></p>
-			<p>&copy;2023 Vancouver Live</p>
-		</section>
+			<section class="site-copyright">
+				<p><a href="#">Privacy Policy</a> | <a href="#">Terms and Conditions</a> | <a href="#">Contact Us</a></p>
+				<p>Built by <a href="#">Taylor Hillier</a>, <a href="#">Zeinab Kordeh</a>, <a href="#">Justin Yu</a>, and <a href="#">Bruce Gerona</a></p>
+				<p>&copy;2023 Vancouver Live</p>
+			</section>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
