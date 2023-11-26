d528e710654c7ce40d63234b7aae7da1435ec805
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-11-25 18:22:30 -0800

    added handling for performers page

--- template-parts/content-vanlive-performer.php
+++ template-parts/content-vanlive-performer.php
@@ -10,27 +10,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  
+
+    <div class="performer-text">
         <?php
-    if ( is_singular() ) :
+        if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+
+        if(function_exists ('get_field')){
+            $about_artist = get_field('about_artist');
+            if($about_artist){
+                $performer_tier = get_field('performer_status');
+                if($performer_tier === 'Headliner'){
+                    echo '<div>';
+                    echo '<p>' . esc_html($about_artist) . '</p>';
+                    echo '</div>';
+                }
+            }
+        }
         ?>
+    </div>
 
     <div class="entry-content">
         <?php   
         the_post_thumbnail();
-        if(function_exists ('get_field')){
-            $about_artist = get_field('about_artist');
-            if($about_artist){
-                echo '<div>';
-                echo '<p>' . esc_html($about_artist) . '</p>';
-                echo '</div>';
-                
-            }
-        }
         ?>
     </div>
 </article>
